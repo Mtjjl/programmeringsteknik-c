28ef1d2b62a94331ec68bbac4cded274ac80d221
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2020-09-01 09:18:10 +0200

    flagApp  & WordsApp skapad

---
+++ Words/Words/WordsApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+             * skriv en konsolappapplikation som tar emot en skriven text 
+             * 
+             * vill ha följande;
+             * Anntal ord?
+             * antal vokaler?
+             * vilket är det längsta ordet
+             * 
+             * Word count
+             * vowel count
+             * longest word*/
+        }
+    }
+}
---
+++ flag/flag/flagApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace flagApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
